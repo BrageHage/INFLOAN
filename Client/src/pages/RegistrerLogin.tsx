cd0b7260a37716e0bcb4130ee4223dbfc40b3a07
BrageHage <brage_hagelund@outlook.com>
2024-06-04 17:38:31 +0200

    Lagt til confirmcomponent når du registrer bruker

--- Client/src/pages/RegistrerLogin.tsx
+++ Client/src/pages/RegistrerLogin.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { login, registrerUser } from "../utils/functions";
+import ConfirmationComponent from "../components/confirmationComponent";
 
 export const RegistrerLogin = () => {
   const [makeUser, setMakeUser] = useState(false);
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleRegister = async () => {
     if (password === repeatPassword && password !== "" && username !== "") {
       try {
         const status = await registrerUser(username, password);
         if (status.status === 200 || status.status === 201) {
-          window.location.reload(); // Last inn siden på nytt etter vellykket registrering
+          setShowConfirmation(true);
+          console.log(showConfirmation);
+          const timer = setTimeout(() => {
+            window.location.reload();
+            setShowConfirmation(false);
+          }, 1500);
+          return () => clearTimeout(timer);
         } else {
           console.error(`Error: ${status.statusText}`);
         }
@@ -43,7 +51,10 @@ export const RegistrerLogin = () => {
   };
 
   return (
-    <div className="flex flex-col items-center w-full h-[60vh] p-4 mt-8">
+    <div className="flex flex-col items-center w-full h-screen p-4 mt-8">
+      {showConfirmation && (
+        <ConfirmationComponent message="Brukeren er blitt oprettet!" />
+      )}
       {makeUser ? (
         <>
           <div className="w-full max-w-md p-6 bg-white border border-gray-300 rounded-lg shadow-sm">
