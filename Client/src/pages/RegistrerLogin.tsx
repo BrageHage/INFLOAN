826a6ed074bd7c132fd09149dc21aa549b59798c
BrageHage <brage_hagelund@outlook.com>
2024-06-04 17:41:42 +0200

    Fjernet unødvendig import av useEffect

--- Client/src/pages/RegistrerLogin.tsx
+++ Client/src/pages/RegistrerLogin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { login, registrerUser } from "../utils/functions";
 import ConfirmationComponent from "../components/confirmationComponent";
 
